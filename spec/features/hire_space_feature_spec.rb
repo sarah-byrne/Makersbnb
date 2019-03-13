cda66aa1547a6650a44101693f84cf07a14dc876
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-03-13 10:41:18 +0000

    update hire feature test to use data from setup

--- spec/features/hire_space_feature_spec.rb
+++ spec/features/hire_space_feature_spec.rb
@@ -1,14 +1,14 @@
 feature 'Book a space' do
     scenario 'click a button to book a space' do
-      Space.create(:name => "Oregon Heights", :description => "Nice space", :price => "loads", :date => "10/10/1900")
       visit '/spaces'
       expect(page).to have_button('Book this space')
     end
 
     scenario 'see a confirmation' do
-      Space.create(:name => "Oregon Heights", :description => "Nice space", :price => "loads", :date => "10/10/1900")
+      name = Space.first().name
       visit '/spaces'
       click_button('Book this space')
-      expect(page).to have_content('You have booked Oregon Heights')
+      msg = "You have booked #{name}"
+      expect(page).to have_content(msg)
     end
 end
