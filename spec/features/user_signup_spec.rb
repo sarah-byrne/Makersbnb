f759b6ea13622fbd4574b23ab5b70fecc39f688c
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-03-13 14:11:16 +0000

    refactor names of input fields in index and feature test for sign up

--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -1,22 +1,22 @@
 feature 'User can sign up' do
   scenario 'it shows input field for username' do
     visit '/'
-    expect(page).to have_field('Username')
+    expect(page).to have_field('username')
   end
 
   scenario 'it shows input field for email address' do
     visit '/'
-    expect(page).to have_field('Email address')
+    expect(page).to have_field('email_address')
   end
 
   scenario 'it shows input field for password' do
     visit '/'
-    expect(page).to have_field('Password')
+    expect(page).to have_field('password')
   end
 
   scenario 'it shows input for password confirmation' do
     visit '/'
-    expect(page).to have_field('Password confirmation')
+    expect(page).to have_field('password_confirmation')
   end
 
   scenario 'it shows sign up button' do
