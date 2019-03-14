ae7d79ca9f53767a558497c70ad01627f182b316
“Brenda” <odonnell.brenda@gmail.com>
2019-03-14 17:09:27 +0000

    Adds a calendar that you can pick from.

--- spec/features/book_some_dates_feature_spec.rb
+++ spec/features/book_some_dates_feature_spec.rb
@@ -15,4 +15,15 @@ feature 'Select some dates' do
     expect(page).to have_field('enddate')
   end
 
+  scenario 'it has a submit button for sending the dates' do
+    visit '/'
+    click_button('View available spaces')
+    click_button('Book this space')
+    fill_in 'startdate', with: "14/03/2019"
+    fill_in 'enddate', with: "17/03/2019"
+    click_button('Submit')
+    expect(page).to have_content("14/03/2019")
+    expect(page).to have_content("17/03/2019")
+  end
+
 end
