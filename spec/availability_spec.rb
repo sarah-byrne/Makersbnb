1d4489dceb6a013d9609ce8fd9a1aaa96a128d03
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-03-14 14:57:22 +0000

    started writing first unit test for Availability class

---
+++ spec/availability_spec.rb
@@ -0,0 +1,9 @@
+require './lib/availability'
+
+describe Availability do
+    it 'can persist a date that a space is available' do
+        # start from an empty Availability table
+        # create a new availability entry
+        # check that the entry is in the table
+    end
+end
