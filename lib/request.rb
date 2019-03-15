d8740c966241758183fe080fa5ea1c6d6f58a9c3
“Brenda” <odonnell.brenda@gmail.com>
2019-03-15 10:32:53 +0000

    it stores start date and end date

--- lib/request.rb
+++ lib/request.rb
@@ -1,6 +1,25 @@
 require 'rubygems'
 require 'data_mapper'
 
+if ENV['ENVIRONMENT'] == 'test'
+  env = "sqlite://#{Dir.pwd}/makers_bnb_test.rb"
+else
+  env = "sqlite://#{Dir.pwd}/makers_bnb.rb"
+end
+
+DataMapper.setup(:default, env)
+
 class Request
 
+  include DataMapper::Resource
+
+  property :id,         Serial
+  property :space_id,   Text
+  property :start_date, String
+  property :end_date,   String
+
+
 end
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
